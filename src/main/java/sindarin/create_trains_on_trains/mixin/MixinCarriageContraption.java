ab4d024bf0990ed4a8af5205ffa6bce24dfe9067
Sindarin27 <sindarin27@gmail.com>
2023-09-04 23:29:07 +0200

    Fix an issue where the second bogey was treated incorrectly due to the first bogey failing to assemble

--- src/main/java/sindarin/create_trains_on_trains/mixin/MixinCarriageContraption.java
+++ src/main/java/sindarin/create_trains_on_trains/mixin/MixinCarriageContraption.java
@@ -1,14 +1,11 @@
 package sindarin.create_trains_on_trains.mixin;
 
 import com.simibubi.create.content.contraptions.Contraption;
-import com.simibubi.create.content.trains.bogey.AbstractBogeyBlock;
 import com.simibubi.create.content.trains.entity.CarriageContraption;
-import com.simibubi.create.foundation.utility.Debug;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
@@ -18,7 +15,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = CarriageContraption.class, remap = false)
 public abstract class MixinCarriageContraption extends Contraption {
@@ -27,7 +23,9 @@ public abstract class MixinCarriageContraption extends Contraption {
     @Inject(method = "capture", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/simibubi/create/content/trains/bogey/AbstractBogeyBlock;captureBlockEntityForTrain()Z"), cancellable = true)
     private void addNewBogey(Level world, BlockPos pos, CallbackInfoReturnable<Pair<StructureTemplate.StructureBlockInfo, BlockEntity>> cir){
         BlockState blockState = world.getBlockState(pos);
-
+        if (pos == this.anchor) return; // This is the anchor; no intervention needed. Return from the mixin and let the original code do its job.
+        
+        // Check if the bogey is in the same direction as we expect true carriage bogeys to be
         Vec3i bogeyDir = this.anchor.subtract(pos);
         boolean isInLine = true;
         
@@ -40,6 +38,7 @@ public abstract class MixinCarriageContraption extends Contraption {
             }
         }
         
+        // If the bogey is not in line, treat it like a normal structure block and return the original method
         if (!isInLine) {
             cir.setReturnValue(Pair.of(new StructureTemplate.StructureBlockInfo(pos, blockState, getBlockEntityNBT(world, pos)),
                     world.getBlockEntity(pos)));
